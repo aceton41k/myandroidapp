887e669d2e5e73463d0d02796dadd6f6fe1fe584
aceton41k <aceton41k@gmail.com>
2017-07-28 14:44:43 +0300

    weeeee

--- app/src/main/java/com/example/saushkin/saushkinandroidapp/MainActivity.java
+++ app/src/main/java/com/example/saushkin/saushkinandroidapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.saushkin.saushkinandroidapp;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,6 +14,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
@@ -20,6 +22,7 @@ public class MainActivity extends AppCompatActivity
 
     private Button myButton;
     private TextView myTextview;
+    private EditText inputText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,10 +51,21 @@ public class MainActivity extends AppCompatActivity
 
         myButton = (Button) findViewById(R.id.myButton);
         myTextview = (TextView) findViewById(R.id.myTextview);
+        inputText = (EditText) findViewById(R.id.inputText);
+        inputText.setText("Started");
+
+
         myButton.setOnClickListener(onClickListener);
         myTextview.setOnClickListener(onClickListener);
 
 
+
+    }
+
+    @Override
+    public void onPostResume() {
+        super.onPostResume();
+        inputText.setText("Resumed");
     }
 
     @Override
@@ -117,7 +131,7 @@ public class MainActivity extends AppCompatActivity
         public void onClick(View view) {
             switch(view.getId()) {
                 case R.id.myButton:
-                    myTextview.setText("Hiii!");
+                    myTextview.setText(inputText.getText());
                     break;
                 case R.id.myTextview:
                     myTextview.setText("Hello World!");
